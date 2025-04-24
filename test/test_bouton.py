697e2077f890ff11629b081beff984a8fb0d7d85
Ashank <Bansiashank2006@gmail.com>
2025-04-24 19:36:38 -0400

    Test unitaires

---
+++ test/test_bouton.py
@@ -0,0 +1,30 @@
+import unittest
+from gpiozero import Device
+from gpiozero.pins.mock import MockFactory
+
+from view.platine import Platine  
+
+Device.pin_factory = MockFactory()
+
+class TestPlatineTP4(unittest.TestCase):
+    def setUp(self):
+        self.platine = Platine()
+
+    def test_bouton1_presse(self):
+        self.platine.buttons["Bouton1"].pin.drive_low()
+        self.assertTrue(self.platine.buttons["Bouton1"].is_active)
+
+    def test_bouton2_presse(self):
+        self.platine.buttons["Bouton2"].pin.drive_low()
+        self.assertTrue(self.platine.buttons["Bouton2"].is_active)
+
+    def test_bouton1_is_presse_false(self):
+        self.platine.buttons["Bouton1"].pin.drive_high()
+        self.assertFalse(self.platine.buttons["Bouton1"].is_active)
+
+    def test_bouton2_is_presse_false(self):
+        self.platine.buttons["Bouton2"].pin.drive_high()
+        self.assertFalse(self.platine.buttons["Bouton2"].is_active)
+
+if __name__ == "__main__":
+    unittest.main()
