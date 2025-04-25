c2a2ab5d18485adc7136b6092e905e0e9b1854a8
ashank-bansi <bansiashank2006@gmail.com>
2025-04-24 21:29:15 -0400

    Fin TP4

--- test/test_bouton.py
+++ test/test_bouton.py
@@ -1,8 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
+#astuce Python pratique pour pouvoir importer des modules dans d'autres dossiers (comme view/) sans casser l'organisation du projet.
+
 import unittest
 from gpiozero import Device
 from gpiozero.pins.mock import MockFactory
 
-from view.platine import Platine  
+from view.platine import Platine
+
 
 Device.pin_factory = MockFactory()
 
